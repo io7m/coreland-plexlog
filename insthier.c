37ca23e963e0bd3f2515df8a5c773e5e88ee6886
m0 <m0@localhost>
2008-05-15 21:40:07 +0000

    config update

--- insthier.c
+++ insthier.c
@@ -7,8 +7,10 @@ struct install_item insthier[] = {
   {INST_MKDIR, 0, 0, ctxt_dlibdir, 0, 0, 0755},
   {INST_MKDIR, 0, 0, ctxt_slibdir, 0, 0, 0755},
   {INST_MKDIR, 0, 0, ctxt_repos, 0, 0, 0755},
+  {INST_COPY, "dir_stack.c", 0, ctxt_repos, 0, 0, 0644},
   {INST_COPY, "fd_lock.c", 0, ctxt_repos, 0, 0, 0644},
   {INST_COPY, "plexlog-conf.c", 0, ctxt_repos, 0, 0, 0644},
+  {INST_COPY, "plexlog.c", 0, ctxt_repos, 0, 0, 0644},
   {INST_COPY, "px_close.c", 0, ctxt_repos, 0, 0, 0644},
   {INST_COPY, "px_fmt.c", 0, ctxt_repos, 0, 0, 0644},
   {INST_COPY, "px_level.c", 0, ctxt_repos, 0, 0, 0644},
@@ -16,6 +18,8 @@ struct install_item insthier[] = {
   {INST_COPY, "px_log.c", 0, ctxt_repos, 0, 0, 0644},
   {INST_COPY, "px_open.c", 0, ctxt_repos, 0, 0, 0644},
   {INST_COPY, "px_rotate.c", 0, ctxt_repos, 0, 0, 0644},
+  {INST_COPY, "dir_stack.h", 0, ctxt_repos, 0, 0, 0644},
+  {INST_COPY, "dir_stack.h", 0, ctxt_incdir, 0, 0, 0644},
   {INST_COPY, "fd_lock.h", 0, ctxt_repos, 0, 0, 0644},
   {INST_COPY, "fd_lock.h", 0, ctxt_incdir, 0, 0, 0644},
   {INST_COPY, "plexlog.h", 0, ctxt_repos, 0, 0, 0644},
@@ -24,5 +28,7 @@ struct install_item insthier[] = {
   {INST_COPY, "plexlog.a", "libplexlog.a", ctxt_slibdir, 0, 0, 0644},
   {INST_COPY, "plexlog-conf.ld", 0, ctxt_repos, 0, 0, 0644},
   {INST_COPY, "plexlog-conf", 0, ctxt_bindir, 0, 0, 0755},
+  {INST_COPY, "plexlog.ld", 0, ctxt_repos, 0, 0, 0644},
+  {INST_COPY, "plexlog", 0, ctxt_bindir, 0, 0, 0755},
 };
 unsigned long insthier_len = sizeof(insthier) / sizeof(struct install_item);
